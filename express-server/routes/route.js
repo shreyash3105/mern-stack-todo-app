0a74a0cdbfc6ec2d21dfcf54e5ebbff1fa17b573
Shreyash Amberkhane <shreyashamberkhane@gmail.com>
2022-06-11 22:25:33 +0530

    updated route file

--- express-server/routes/route.js
+++ express-server/routes/route.js
@@ -1,10 +1,6 @@
-// ./express-server/routes/todo.server.route.js
 import express from 'express';
-
-//import controller file
 import * as todoController from '../controllers/controller';
 
-// get an instance of express router
 const router = express.Router();
 
 router.route('/')
